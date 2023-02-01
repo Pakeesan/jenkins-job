06e23d67b2ca15466ae001b5d977e0d3fedce1a2
unknown <sanpakees@gmail.com>
2023-02-01 17:33:15 +0530

    first commit

---
+++ src/main/java/com/ii/jenkins/JenkinsJobProjectApplication.java
@@ -0,0 +1,25 @@
+package com.ii.jenkins;
+
+import java.util.logging.Logger;
+
+import org.apache.juli.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinsJobProjectApplication {
+	
+//	public static Logger logger =(Logger)LogFactory.getLog(JenkinsJobProjectApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("welcome to jenkins ");
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsJobProjectApplication.class, args);
+	}
+
+}
