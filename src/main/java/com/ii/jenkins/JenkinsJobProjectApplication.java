2ba60a226115e89b34209b760d35e70415bad472
unknown <sanpakees@gmail.com>
2023-02-02 10:26:43 +0530

    logger

--- src/main/java/com/ii/jenkins/JenkinsJobProjectApplication.java
+++ src/main/java/com/ii/jenkins/JenkinsJobProjectApplication.java
@@ -3,6 +3,7 @@ package com.ii.jenkins;
 import java.util.logging.Logger;
 
 import org.apache.juli.logging.LogFactory;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +12,13 @@ import jakarta.annotation.PostConstruct;
 @SpringBootApplication
 public class JenkinsJobProjectApplication {
 	
-//	public static Logger logger =(Logger)LogFactory.getLog(JenkinsJobProjectApplication.class);
+	public static org.slf4j.Logger logger=LoggerFactory.getLogger(JenkinsJobProjectApplication.class);
 	
 	@PostConstruct
 	public void init() {
 		System.out.println("Hello");
 		System.out.println("welcome to jenkins ");
+		logger.info("hello");
 	}
 	
 	public static void main(String[] args) {
