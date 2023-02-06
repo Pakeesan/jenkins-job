e09c3ddc729d7632cc15b2e2be2983472d5cf196
unknown <sanpakees@gmail.com>
2023-02-06 09:35:17 +0530

    remove logger

--- src/main/java/com/ii/jenkins/JenkinsJobProjectApplication.java
+++ src/main/java/com/ii/jenkins/JenkinsJobProjectApplication.java
@@ -1,8 +1,5 @@
 package com.ii.jenkins;
 
-import java.util.logging.Logger;
-
-import org.apache.juli.logging.LogFactory;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,13 +9,13 @@ import jakarta.annotation.PostConstruct;
 @SpringBootApplication
 public class JenkinsJobProjectApplication {
 	
-	public static org.slf4j.Logger logger=LoggerFactory.getLogger(JenkinsJobProjectApplication.class);
+//	public static org.slf4j.Logger logger=LoggerFactory.getLogger(JenkinsJobProjectApplication.class);
 	
 	@PostConstruct
 	public void init() {
 		System.out.println("Hello");
 		System.out.println("welcome to jenkins ");
-		logger.info("hello");
+//		logger.info("hello");
 	}
 	
 	public static void main(String[] args) {
